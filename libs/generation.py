4643f1c5a3104e9aae7cb5c01df7a4450a3c1a60
Raz Besaleli <rbesaleli@gmail.com>
2023-03-12 16:43:40 +0400

    update

--- libs/generation.py
+++ libs/generation.py
@@ -1,4 +1,5 @@
-from typing import Union
+from typing import Union, List
+from dataclasses import dataclass
 import pandas as pd
 import torch
 from transformers import (PreTrainedModel,
@@ -20,16 +21,25 @@ generation_format = """
 
 class EarlyStop(StoppingCriteria):
     def __init__(self, keywords: list, tokenizer: PreTrainedTokenizer):
+        self.keywords = keywords
         self.stop_ids = [tokenizer.encode(w)[0] for w in keywords]
 
     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
         return input_ids[0][-1] in self.stop_ids
 
 
+@dataclass
+class ModelInput:
+    query: str
+    examples: pd.DataFrame
+
+
 class DescriptionGenerator:
     def __init__(self, model: PreTrainedModel, tokenizer: PreTrainedTokenizer):
         self.model = model
         self.tokenizer = tokenizer
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+        self.tokenizer.padding_side = 'left'
         
     generation_format = generation_format.strip()
         
@@ -48,18 +58,55 @@ class DescriptionGenerator:
         formatted_examples = examples.apply(self.format_example, axis=1).to_list()
         
         return '\n\n'.join(formatted_examples + [formatted_query])
+    
+    def remove_prompt_from_generation(self, output: torch.LongTensor, tokenized_prompt: dict):
+        return [i[len(j):] for i, j in zip(output, tokenized_prompt['input_ids'])]
+    
+    def postproc_early_stop(self, generations: List[str], early_stop: Union[EarlyStop, None] = None):
+        if early_stop is None:
+            return generations
         
-    def generate_description(self, query: str, examples: Union[pd.DataFrame, pd.Series, None] = None, generation_kwargs: Union[None, dict] = None):
-        prompt = self.create_prompt(query, examples)
+        truncated_generations = generations 
         
-        tokenized_prompt = self.tokenizer(prompt, return_tensors='pt').to(self.model.device)
+        for keyword in early_stop.keywords:
+            truncated_generations = [i[:-len(keyword)] if i.endswith(keyword) else i for i in truncated_generations]
+            
+        return truncated_generations
+    
+    def generate_description(self, model_inputs: List[ModelInput], generation_kwargs: Union[None, dict] = None):
+    # def generate_description(self, query: str, examples: Union[pd.DataFrame, pd.Series, None] = None, generation_kwargs: Union[None, dict] = None):
+        prompts = [
+            self.create_prompt(model_input.query, model_input.examples) for model_input in model_inputs
+            ]
+        
+        tokenized_inputs = self.tokenizer(
+            prompts,
+            return_tensors='pt',
+            padding=True,
+            truncation=True
+            ).to(self.model.device)
         
         generation_kwargs = generation_kwargs or dict()
         
         if 'stop' in generation_kwargs:
-            generation_kwargs['stopping_criteria'] = StoppingCriteriaList([EarlyStop(generation_kwargs.pop('stop'), self.tokenizer)])
+            early_stop = EarlyStop(generation_kwargs.pop('stop'), self.tokenizer)
+            generation_kwargs['stopping_criteria'] = StoppingCriteriaList([early_stop])
+        else:
+            early_stop = None
+        
+        if 'echo' in generation_kwargs:
+            echo = generation_kwargs.pop('echo')
+        else:
+            echo = False
         
         with torch.no_grad():
-            output = self.model.generate(**tokenized_prompt, **generation_kwargs)
+            output = self.model.generate(**tokenized_inputs, **generation_kwargs)
         
-        return self.tokenizer.decode(output.squeeze())
+        if not echo:
+            output = self.remove_prompt_from_generation(output, tokenized_inputs)
+        
+        decoded_generation = [self.tokenizer.decode(i) for i in output]
+        
+        decoded_generation = self.postproc_early_stop(decoded_generation, early_stop=early_stop)
+        
+        return decoded_generation
--- test_model.py
+++ test_model.py
@@ -2,7 +2,7 @@ import datasets
 from transformers import (GPT2LMHeadModel, AutoTokenizer)
 
 from libs.collection import RandomCollection
-from libs.generation import DescriptionGenerator
+from libs.generation import DescriptionGenerator, ModelInput
 
 dataset = datasets.load_dataset('sede')
 
@@ -13,14 +13,19 @@ model = DescriptionGenerator(
     tokenizer=AutoTokenizer.from_pretrained('gpt2')
     )
 
+test_examples = collection.retrieve(dataset['test'].to_pandas().iloc[0], 3)
+
+model_inputs = [
+    ModelInput(dataset['test'][i]['QueryBody'], test_examples) for i in range(5)
+    ]
+
 gen = model.generate_description(
-    dataset['test'][0]['QueryBody'],
-    collection.retrieve(dataset['test'].to_pandas().iloc[0], 3),
+    model_inputs,
     generation_kwargs=dict(
         max_new_tokens=64,
-        do_sample=True,
-        top_p=0.95,
-        stop=['\n', '#'])
+        # do_sample=True,
+        # top_p=0.95,
+        stop=['#'])
     )
 
 print(gen)
